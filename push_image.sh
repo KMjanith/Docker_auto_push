36df73dff0ef3e55e65479979d323354673e038e
KMjanith <kavinduj.20@cse.mrt.ac.lk>
2025-01-16 15:40:20 +0530

    added version update sstrategy (patch)

--- push_image.sh
+++ push_image.sh
@@ -18,15 +18,32 @@ else
     # Split the latest tag into parts (assuming the format is major.minor.patch)
     IFS='.' read -r MAJOR MINOR PATCH <<<"${LATEST_TAG//[^0-9.]/}"
 
-    # Increment the PATCH version
-    MINOR=$((MINOR + 1))
+    # Increment version based on the argument
+    case $1 in
+        major)
+            MAJOR=$((MAJOR + 1))
+            MINOR=0
+            PATCH=0
+            ;;
+        minor)
+            MINOR=$((MINOR + 1))
+            PATCH=0
+            ;;
+        patch)
+            PATCH=$((PATCH + 1))
+            ;;
+        *)
+            echo "Usage: $0 {major|minor|patch}"
+            exit 1
+            ;;
+    esac
 
     # Create the new tag
     NEW_TAG="$MAJOR.$MINOR.$PATCH"
 fi
 
 # Build the image with the new tag
-docker build -f Dockerfile -t $IMAGE_REPO:$NEW_TAG .
+docker build -f Dockerfile.prod -t $IMAGE_REPO:$NEW_TAG .
 
 # Push the new image to Docker Hub
 docker push $IMAGE_REPO:$NEW_TAG
